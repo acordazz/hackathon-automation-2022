bcde59152bc4cdbfb6bcf3e955c9d1154db119a2
André Cordazzo Brunelli <andre.cordazzo-brunelli@sogeti.no>
2022-10-18 18:20:46 +0200

    added test files separated by difficulty

---
+++ tests/enkel.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect, Page } from '@playwright/test';
+import { Home } from '../POM/home';
+import { Sidebar } from '../POM/sidebar';
+
+let p: Page;
+
+test.beforeEach(async ({ page }) => {
+    page.goto("https://hackatonwebapp.azurewebsites.net/");
+    p = page;
+});
+
+test('1. Lag et script som navigerer seg igjennom hele nettområdet. Scriptet skal klikke på alle menyvalgene', async () => {
+    const home = new Home(p);
+    const sidebar = new Sidebar(p);
+    await sidebar.toDo.click();
+    await sidebar.counter.click();
+    await sidebar.fetchData.click();
+    await sidebar.hackathonWebApp.click();
+    await sidebar.home.click();
+});
