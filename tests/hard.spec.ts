3f9540ce23e7ac4a1fdb1db58fbfc2f68b36be41
André Cordazzo Brunelli <andre.cordazzo-brunelli@sogeti.no>
2022-10-26 13:06:32 +0200

    tests whether we get sound on drums

--- tests/hard.spec.ts
+++ tests/hard.spec.ts
@@ -60,14 +60,37 @@ test('Hard: 2. Lag et script som enabler/trykker på checkbox for alle item du l
     
 });
 
-test('Hard: 3.	Lag et script som trykker på trommen i siden som det lenkes til i den grå boksen.', async () => {
+test('Hard: 3.	Lag et script som trykker på trommen i siden som det lenkes til i den grå boksen.', async ({ request }) => {
     const home = new Home(p);
     let page1;
     await test.step(`Clicking on link to drums page and waiting for it to load.`, async () => {
         page1 = await home.clickOnDrumsLink();
     });
     expect(page1, 'Verifying that popup for drums is opened').not.toBeNull();
-    await test.step(`Added a small timeout to record drums.`, async () => {
-        await p.waitForTimeout(3 * 1000);
+    await test.step(`Testing whether we receive sound of drums.`, async () => {
+        type req = {
+            method: string, url: string,
+        };
+        type res = {
+            status: string, url: string,
+        }
+        const requests: req[] = [];
+        const responses: res[] = [];
+        page1.on('request', request =>
+            {
+                console.log('>>', request.method(), request.url());
+                requests.push({method: request.method(), url: request.url()});
+        });
+        page1.on('response', response =>
+            {
+                console.log('<<', response.status(), response.url());
+                responses.push({status: response.status(), url: response.url()});
+            });
+        const response = await page1.getByRole('img', { name: 'Tromme' }).click();
+        const soundFound = responses.find((obj) => {
+            return obj.status == "206" && obj.url == "https://trommelyd.no/media/trommelyd.mp3"; 
+        }) != undefined;
+        expect(soundFound, `Verifying whether clicking on drums gets trommelyd.mp3 as response`).toBeTruthy();
+
     });
 });
