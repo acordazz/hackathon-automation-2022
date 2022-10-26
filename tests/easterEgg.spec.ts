6237f4c2058a0965da8206bec3188588528ca11b
Mikael Kvaal <mikael.kvaal@sogeti.com>
2022-10-26 10:36:37 +0200

    added easter egg test

---
+++ tests/easterEgg.spec.ts
@@ -0,0 +1,23 @@
+import { test, Page, Locator, expect } from '@playwright/test';
+import { Sidebar } from '../POM/sidebar';
+import { Counter } from '../POM/Counter';
+
+let p: Page;
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("https://hackatonwebapp.azurewebsites.net/");
+    p = page;
+});
+
+
+test("EasterEgg?", async ({page}) => {
+            const counter = new Counter(p);
+            const sidebar = new Sidebar(p);
+            await sidebar.counter.click();
+            await page.waitForTimeout(1500)
+            
+            for(let i = 0; i<44; i++){
+                await counter.counterClickMe.click()
+            }
+            await expect(counter.currentCount).toContainText("100001");
+})
